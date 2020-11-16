681230a3433cd30a8bf546d2350ef8322c4a166d
Connor Lydon <connoralydon@gmail.com>
2020-11-15 17:38:50 -0800

    shellcheck

--- username.sh
+++ username.sh
@@ -6,14 +6,14 @@ echo "Use lower case characters, digits, & underscore character"
 echo " It must start with a lower case character"
 echo " It must contain at least 3 but not more than 12 characters"
 echo " Enter the username: "
-read USERNAME
-while echo "$USERNAME" | egrep -v "^[a-z][0-9a-z_]{3,12}$" > /dev/null 2>&1
+read -r USERNAME
+while echo "$USERNAME" | grep -Ev "^[a-z][0-9a-z_]{3,12}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username"
 	echo "Use lower case characters, digits, & underscore character"
 	echo " It must start with a lower case character"
 	echo " It must contain at least 3 but not more than 12 characters"
 	echo "Enter a valid username: "
-	read USERNAME
+	read -r USERNAME
 done
 echo "Thank you"
